bf946d1ff0a85cebf91e7d8a41dee22b8517c5a1
codecademy <ccuser@codecademy.com>
2016-12-22 14:12:11 -0500

    Add contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,5 @@ class PagesController < ApplicationController
   def welcome; end
   def portfolio; end
   def about; end
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get '/portfolio' => 'pages#portfolio', as: :portfolio
   get '/about' => 'pages#about', as: :about
+  get '/contact' => 'pages#contact', as: :contact
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
